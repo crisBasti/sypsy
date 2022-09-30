889df2f3a5948c23f2d57f9e9982ed22ade0edc1
crisbasti <crisporti1988@gmail.com>
2022-09-29 22:19:12 -0300

    ultimando detalles para lanzamiento

---
+++ db_productosSericios.js
@@ -0,0 +1,4 @@
+const productosServicios = [
+    {id: 1, nombre: "Refrigeracion", tipo: "Refrigeracion", cantidad: 1, desc: "Todo tipo de trabajo en refrigeracion y calefaccion . Residencial , de oficinas y Comercial", precio: "a convenir", tel: 1125406044, img:"../imagenes/imgServicios/banner_onfire.jpeg", marca:""},
+    {id: 2, nombre: "Sacabollos", tipo: "Sacabollos", cantidad: 1, desc: "Todo tipo de bollos, granizo, golpes etc", precio: "a convenir", tel: 1162296128, img:"../imagenes/imgServicios/sacabollo_juakko.jpg", marca:""}
+]
