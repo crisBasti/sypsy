9592bb5459a772d5fae588b23b7243ff7cfe21e0
crisbasti <crisporti1988@gmail.com>
2022-10-19 09:58:49 -0300

    agregando servicios y variacion del dolar

--- db_productosSericios.js
+++ db_productosSericios.js
@@ -1,4 +1,5 @@
 const productosServicios = [
     {id: 1, nombre: "Refrigeracion", tipo: "Refrigeracion", cantidad: 1, desc: "Todo tipo de trabajo en refrigeracion y calefaccion . Residencial , de oficinas y Comercial", precio: "a convenir", tel: 1125406044, img:"../imagenes/imgServicios/banner_onfire.jpeg", marca:""},
-    {id: 2, nombre: "Sacabollos", tipo: "Sacabollos", cantidad: 1, desc: "Todo tipo de bollos, granizo, golpes etc", precio: "a convenir", tel: 1162296128, img:"../imagenes/imgServicios/sacabollo_juakko.jpg", marca:""}
+    {id: 2, nombre: "Sacabollos", tipo: "Sacabollos", cantidad: 1, desc: "Todo tipo de bollos, granizo, golpes etc", precio: "a convenir", tel: 1162296128, img:"../imagenes/imgServicios/sacabollo_juakko.jpg", marca:""},
+    {id: 3, nombre: "Alelux Publicidad", tipo: "Letreros Luminosos", cantidad: 1, desc: "Letreros Neon Marquesinas Vidriera Pintura Electricidad", precio: "a convenir", tel: 1563520865, img:"../imagenes/imgServicios/letreros_luminosos.jpg", marca:""}
 ]
