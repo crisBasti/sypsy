dec1568291eb66a3fa4cacf93b5c7db1ac01de6c
crisbasti <crisporti1988@gmail.com>
2022-10-18 22:25:02 -0300

    agregando productos indu

--- db_productosIndu.js
+++ db_productosIndu.js
@@ -2,16 +2,15 @@ const productosIndu = [
     {id: 1, nombre: "Remera Reebok", tipo: "Remera", cantidad: 1, desc: "Nueva talle 'L', color negro,", precio: 11000, talle: "L", img:"../imagenes/imgIndumentaria/remera_negra_reebok.jpeg", marca:"REEBOK"},
     {id: 2, nombre: "remera Nike", tipo: "Remera", cantidad: 1, desc: "Color rojo, Nueva talle 'L'", precio: 24200, talle: "L", img:"../imagenes/imgIndumentaria/remera_roja_nike.jpeg", marca:"Inova"},
     {id: 3, nombre: "Jeans Zara", tipo: "Pantalon", cantidad: 1, desc: "Color azul clasico,con roturas al frente. Nuevo talle '48'", precio: 27500, talle: "", img:"../imagenes/imgIndumentaria/vaquero_zara.jpeg", marca:"Mobile"},
-    {id: 4, nombre: "pantalon Nike", tipo: "Pantalon", cantidad: 1, desc: "Color azul con rojo. Nuevo Talle 'M'", precio: 30800, talle: "M", img:"../imagenes/imgIndumentaria/pantalon_nike.jpeg", marca:"Apple"}
-]
-
-
-//     {id: 5, nombre: "Prolongador Multiple", tipo: "Extencion usb", cantidad: 1, desc: "Color blanco, se conecta con cable, 4 puertos USB 3.0 , 1 toma corriente plano triple. Nuevo en caja", precio: 650, talle: "", img:"../imagenes/imgElectro/prolongadorInova.jpeg", marca:"Inova"},
-//     {id: 6, nombre: "Foco bolichero", tipo: "Lampara", cantidad: 1, desc: "proyecta luces de Colores, se conecta a rosca . Nuevo en caja", precio: 500, talle: "", img:"../imagenes/imgElectro/lamparaLuminosa.jpeg", marca:"Crosley"},
-//     {id: 7, nombre: "Auriculares Wireless P47", tipo: "auricular bluetooth", cantidad: 1, desc: "Color negro, se conecta con cable, via bluetooth. se carga con mini USB. Nuevo en caja", precio: 1800, talle: "", img:"../imagenes/imgElectro/AuricularP47.jpeg", marca:"P47"},
-//     {id: 8, nombre: "Termometro Digital", tipo: "digital", cantidad: 1, desc: "Color blanco, funciona a pilas. Nuevo en caja", precio: 500, talle: "", img:"../imagenes/imgElectro/termometroDigital.jpeg", marca:"China"},
-//     {id: 9, nombre: "Parlante mini", tipo: "Parlante", cantidad: 1, desc: "Colores varios, se conecta con cable, via bluetooth. Nuevo en caja", precio: 500, talle: "", img:"../imagenes/imgElectro/paarlantesMini.jpeg", marca:"China"},
-//     {id: 10, nombre: "Balanza de Precision", tipo: "Digital", cantidad: 1, desc: "Color Gris, se carga con cable mini usb. Nuevo en caja", precio: 2000, talle: "", img:"../imagenes/imgElectro/balanzaPrecision.jpeg", marca:"Shirka"},
-//     {id: 11, nombre: "Laser larga distancia", tipo: "Led", cantidad: 1, desc: "Color negro, fnciona a pilas. Nuevo en caja", precio: 800, talle: "", img:"../imagenes/imgElectro/laser.jpeg", marca:"China"},
-//     {id: 12, nombre: "Auriculares Y47", tipo: "Auricular", cantidad: 1, desc: "Color rosa, se conecta con cable, via Bluetooth. Se carga con mini usb. Nuevo en caja", precio: 2500, talle: "", img:"../imagenes/imgElectro/auricularesY47.jpeg", marca:"Y47"},
-//     {id: 13, nombre: "Teclado weibo Luminoso", tipo: "kit luminoso", cantidad: 1, desc: "Kit gamer. Teclado y mouse luminosos. Teclado estatico y mouse retro Luminico. Nuevo en caja", precio: 4500, talle: "", img:"../imagenes/imgElectro/Teclado-Weibo.jpeg", marca:"Y47"}
+    {id: 4, nombre: "pantalon Nike", tipo: "Pantalon", cantidad: 1, desc: "Color azul con rojo. Nuevo Talle 'M'", precio: 30800, talle: "M", img:"../imagenes/imgIndumentaria/pantalon_nike.jpeg", marca:"Apple"},
+    {id: 5, nombre: "Blusa Rayada", tipo: "Remera", cantidad: 1, desc: "Color blanco con rosa. Usada. Talle 1", precio: 300, talle: "1", img:"../imagenes/imgIndumentaria/blusaRayada_Ingrid.jpeg", marca:"??"},
+    {id: 6, nombre: "Enterito de Jeans", tipo: "Vestido", cantidad: 1, desc: "Nuevo, tipo vestidito de jeans", precio: 600, talle: "40/42", img:"../imagenes/imgIndumentaria/enteritoJeans_Ingrid.jpeg", marca:"??"},
+    {id: 7, nombre: "Remera Azul", tipo: "Remera", cantidad: 1, desc: "Color azul. Nueva", precio: 500, talle: "M", img:"../imagenes/imgIndumentaria/remeraAzul_Ingrid.jpeg", marca:"??"},
+    {id: 8, nombre: "Remera deportiva de Chicago", tipo: "Remera", cantidad: 1, desc: "Color Verde. Esta como nueva", precio: 1000, talle: "L", img:"../imagenes/imgIndumentaria/RemeraChicago.jpeg", marca:"??"},
+    {id: 9, nombre: "Remera Frida", tipo: "Remera", cantidad: 1, desc: "Colores gris. Nueva", precio: 300, talle: "M", img:"../imagenes/imgIndumentaria/remeraGris_FridaIngrid.jpeg", marca:"??"},
+    {id: 10, nombre: "Remera roja", tipo: "Remera", cantidad: 1, desc: "Color rojo. Nueva", precio: 500, talle: "L", img:"../imagenes/imgIndumentaria/remeraRoja_Ingrid.jpeg", marca:"??"},
+    {id: 11, nombre: "Remerita de tiritas", tipo: "Remera", cantidad: 1, desc: "Color rosa. Nuevo", precio: 500, talle: "2", img:"../imagenes/imgIndumentaria/remeritaTiritas_Ingrid.jpeg", marca:"??"},
+    {id: 12, nombre: "Top gris", tipo: "Remera", cantidad: 1, desc: "Color Gris. Usada", precio: 300, talle: "1", img:"../imagenes/imgIndumentaria/topGris_Ingrid.jpeg", marca:"?'"},
+    {id: 13, nombre: "Top rosa", tipo: "Remera", cantidad: 1, desc: "Color Rosa. Usado", precio: 300, talle: "2", img:"../imagenes/imgIndumentaria/topRosa_Ingrid.jpeg", marca:"??"},
+    {id: 14, nombre: "Vestido De Tiritas", tipo: "Vestido", cantidad: 1, desc: "Color naraja con gris. Nuevo", precio: 600, talle: "L", img:"../imagenes/imgIndumentaria/vestidoTiritas_Ingrid.jpeg", marca:"??"},
+]  
