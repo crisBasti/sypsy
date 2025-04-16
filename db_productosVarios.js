02bfe6d56dfd4a0c962ae05ed3ff50601892c208
crisBasti <crisporti1988@gmail.com>
2025-04-16 14:04:20 -0300

    eliminando img viejas

--- db_productosVarios.js
+++ db_productosVarios.js
@@ -4,5 +4,5 @@ const productosVarios = [
     {id: 2, nombre: "Perfume Lattafa Khamrah eau de parfum 100 ml", tipo: "Perfume", cantidad: 1, desc: "Perfume Lattafa Khamrah eau de parfum 100 ml. Nuevo", precio: 82000, talle: "", img:"../imagenes/imgVarios/khamrah_lattafa_100ml.jpg", marca:"Juampi"},
     {id: 3, nombre: "Asad Lattafa Perfume Hombre 100 Ml Eau De Parfum", tipo: "Perfume", cantidad: 1, desc: "Asad Lattafa Perfume Hombre 100 Ml Eau De Parfume. Nuevo", precio: 72000, talle: "", img:"../imagenes/imgVarios/lattafa_asad_100ml_for_men.jpg", marca:"Juampi"},
     {id: 4, nombre: "Lattafa Bade'e Al Oud For Glory EDP Spray Hombre 100 mL", tipo: "Perfume", cantidad: 1, desc: "Lattafa Bade'e Al Oud For Glory EDP Spray Hombre 100 mL. Nuevo", precio: 72000, talle: "", img:"../imagenes/imgVarios/Lattafa_oud_for_glory.jpg", marca:"Juampi"},
-    {id: 5, nombre: "Perfume de mujer Lattafa Yara eau de parfum 100 ml", tipo: "Perfume", cantidad: 1, desc: "Perfume de mujer Lattafa Yara eau de parfum 100 ml. Nueva", precio: 72000, talle: "", img:"../imagenes/imgVarios/lattafa_yara.jpg", marca:"Juampi"}  
+    {id: 5, nombre: "Perfume de mujer Lattafa Yara eau de parfum 100 ml", tipo: "Perfume", cantidad: 1, desc: "Perfume de mujer Lattafa Yara eau de parfum 100 ml. Nueva", precio: 72000, talle: "", img:"../imagenes/imgVarios/lattafa_yara.jpg", marca:"Juampi"}
 ]
