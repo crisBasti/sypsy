dacaa2aa173a3b601a68171a88a248cb1446a9c7
crisBasti <crisporti1988@gmail.com>
2025-04-18 15:23:42 -0300

    Cargando productos

--- db_productosSericios.js
+++ db_productosSericios.js
@@ -3,5 +3,9 @@ const productosServicios = [
     {id: 2, nombre: "Sacabollos", tipo: "Sacabollos", cantidad: 1, desc: "Todo tipo de bollos, granizo, golpes etc", precio: "a convenir", tel: 1162296128, img:"../imagenes/imgServicios/sacabollo_juakko.jpg", marca:""},
     {id: 3, nombre: "Alelux Publicidad", tipo: "Letreros Luminosos", cantidad: 1, desc: "Letreros Neon Marquesinas Vidriera Pintura Electricidad", precio: "a convenir", tel: 1563520865, img:"../imagenes/imgServicios/letreros_luminosos.jpeg", marca:""},
     {id: 4, nombre: "MiniFlete Alan y Moni", tipo: "Remis mini Flete", cantidad: 1, desc: "Reparto para empresas, Encomiendas, Expreso, Remis, Viajes al interior, Costa Atlántica", precio: "a convenir", tel: 1123017759, img:"../imagenes/imgServicios/miniFlete_alanYmony.jpeg", marca:"Alan y Moni"},
-    {id: 5, nombre: "Casino Virtual", tipo: "Casino", cantidad: 1, desc: "Jugá al Mejor Casino Virtual Konabet. Apreta el boton y comenzá con la Diversion.", precio: 1000, tel: 1164521118, img:"../imagenes/imgServicios/KonabetCasino.jpeg", marca:"Konabet"}
+    {id: 5, nombre: "Casino Virtual", tipo: "Casino", cantidad: 1, desc: "Jugá al Mejor Casino Virtual Konabet. Apreta el boton y comenzá con la Diversion.", precio: 1000, tel: 1164521118, img:"../imagenes/imgServicios/KonabetCasino.jpeg", marca:"Konabet"},
+    {id: 6, nombre: "Di Castro Detailing del Automóvil", tipo: "Detailing Auto", cantidad: 1, desc: "Limpieza de tapizados , detalle de interiores.", precio: 10, tel: 1164521118, img:"../imagenes/imgServicios/DiCastro_detailing_publi.jpeg", marca:"ECastro"},
+    {id: 7, nombre: "Servicio de Viajes Particulares", tipo: "Viajes remis Auto Particular", cantidad: 1, desc: "Servicio de Viajes Particulares Ubicado en la Provincia de Salta", precio: 10, tel: 1164521118, img:"../imagenes/imgServicios/Remis_particular_Gonza.jpeg", marca:"GVidal"}
+
+
 ]
